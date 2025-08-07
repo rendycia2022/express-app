093e94940b67f220b1aa857c16b709e46552b641
rendycia2022 <rendy.cia2022@gmail.com>
2025-08-07 04:58:04 -0400

    adjustment controller for new find function

--- app/src/apps/auth/controllers/signUpController.js
+++ app/src/apps/auth/controllers/signUpController.js
@@ -8,32 +8,52 @@ exports.get = (req, res) => {
 };
 
 exports.getById = (req, res) => {
-    const item = service.getById(req.params.id);
+    const item = service.getBy({ id: req.params.id});
     if (!item) return res.status(404).json({ message: 'item not found' });
     res.json(item);
 };
 
 exports.create = (req, res) => {
     const payload = req.body;
-    // Entry ID to new data
-    newItem = {
-        id: uuidv4(),
-        created_at: timestamp(), 
-        updated_at: timestamp(), 
-        ...payload
+
+    // cek apakah data sudah pernah ada?
+    const getData = service.getBy({ email: payload.email});
+    if (!getData) {
+        // jika data kosong maka buat data baru
+        newItem = {
+            id: uuidv4(),
+            created_at: timestamp(), 
+            updated_at: timestamp(), 
+            ...payload
+        }
+        const item = service.create(newItem);
+        return res.status(201).json(item);
     }
-    const item = service.create(newItem);
-    res.status(201).json(item);
+
+    res.status(500).json({message: "item exist"});
 };
 
 exports.update = (req, res) => {
-    const item = service.update(req.params.id, req.body);
+    // data dari url
+    const params = {
+        id: req.params.id
+    }
+
+    // data dari body
+    const payload = {
+        updated_at: timestamp(), 
+        ...req.body
+    };
+    const item = service.update(params, payload);
     if (!item) return res.status(404).json({ message: 'item not found' });
     res.json(item);
 };
 
 exports.delete = (req, res) => {
-    const success = service.delete(req.params.id);
+    const params = {
+        id: req.params.id
+    }
+    const success = service.delete(params);
     if (!success) return res.status(404).json({ message: 'item not found' });
     res.status(204).send();
 };
