da6437dfb151835da2d885f9c36c539e7f365288
rendycia2022 <rendy.cia2022@gmail.com>
2025-08-07 23:54:51 -0400

    new feature hashing password, update delete method

--- app/src/apps/auth/controllers/signUpController.js
+++ app/src/apps/auth/controllers/signUpController.js
@@ -1,6 +1,7 @@
 const service = require('../services/signUpService');
 const { v4: uuidv4 } = require('uuid');
 const {timestamp} = require('../../../middlewares/date');
+const bcrypt = require('bcryptjs');
 
 exports.get = (req, res) => {
     const items = service.getAll();
@@ -13,17 +14,24 @@ exports.getById = (req, res) => {
     res.json(item);
 };
 
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
     const payload = req.body;
 
     // cek apakah data sudah pernah ada?
     const getData = service.getBy({ email: payload.email});
     if (!getData) {
         // jika data kosong maka buat data baru
+
+        // hashing password
+        if(payload.password){
+            payload.password = await hashPassword(payload.password);
+        }
+
         newItem = {
             id: uuidv4(),
             created_at: timestamp(), 
             updated_at: timestamp(), 
+            active: 1,
             ...payload
         }
         const item = service.create(newItem);
@@ -33,7 +41,7 @@ exports.create = (req, res) => {
     res.status(500).json({message: "item exist"});
 };
 
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
     // data dari url
     const params = {
         id: req.params.id
@@ -42,8 +50,15 @@ exports.update = (req, res) => {
     // data dari body
     const payload = {
         updated_at: timestamp(), 
+        active: 1,
         ...req.body
     };
+
+    // hashing password
+    if(payload.password){
+        payload.password = await hashPassword(payload.password);
+    }
+
     const item = service.update(params, payload);
     if (!item) return res.status(404).json({ message: 'item not found' });
     res.json(item);
@@ -53,7 +68,21 @@ exports.delete = (req, res) => {
     const params = {
         id: req.params.id
     }
-    const success = service.delete(params);
+    const payload = {
+        active: 0,
+        updated_at: timestamp(), 
+        ...req.body
+    };
+    const item = service.update(params, payload);
+
+    // Jika dilakukan hard delete
+    // const success = service.delete(params);
     if (!success) return res.status(404).json({ message: 'item not found' });
     res.status(204).send();
 };
+
+async function hashPassword(plainPassword) {
+    const saltRounds = 10; // Semakin besar, semakin aman, tapi lebih lambat
+    const hashed = await bcrypt.hash(plainPassword, saltRounds);
+    return hashed;
+}
